aeecb1ae06fff1eee19a327bc34ba962ef7182a2
Radman <rdmnr@protonmail.com>
2026-04-22 23:38:12 -0700

    Add PostHog for anonymous usage analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import posthog from 'posthog-js'
 import './index.css'
 import './styles/global.css'
 import App from './App.tsx'
+
+posthog.init('phc_R7ZAuPFBNbxkup1BuUERgB7zRw1NC4htBd4Eu9N49MQ', {
+  api_host: 'https://us.i.posthog.com',
+  persistence: 'localStorage+cookie',
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
